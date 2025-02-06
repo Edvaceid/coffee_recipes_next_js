f6c343c3fcbb0ce11863ce3c20f346fa515603e9
Arda Sevik <edvaceid@gmail.com>
2025-02-06 03:09:18 +0300

    tailwind.config.ts duzeltme4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
-import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+
+export default tseslint.config({
+  rules: {
+    "@typescript-eslint/no-require-imports": "error"
+  }
 });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
